199c18e29984da1c042725ba4cac614613b8e56a
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-12 16:41:01 -0500

    feat: implement router configuration with login, register, and analytics routes

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LoginPage from "@/iam/interfaces/pages/login-page.component.vue";
+import RegisterPage from "@/iam/interfaces/pages/register-page.component.vue";
+import MainLayoutPage from "@/shared/pages/main-layout-page.vue";
+import AnalyticsPage from "@/analytics/interfaces/pages/analytics-page.component.vue";
+
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: "/login", component: LoginPage },
+  { path: "/register", component: RegisterPage },
+  {
+    path: "/",
+    component: MainLayoutPage,
+    children: [
+      {
+        path: "home",
+        component: AnalyticsPage,
+        props: true,
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
