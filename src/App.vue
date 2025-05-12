a47eee9a822f6dc5a1e813029539852ce4bf7671
Sebastian Ramirez Hoffmann <u202211894@upc.edu.pe>
2025-05-12 09:56:33 -0500

    chore: primevue config and dependencies for axios and vue router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Button from "primevue/button"
 </script>
 
 <template>
-<h1 class="text-red-500 text-6xl">SEXO</h1>
+  <h1 class="text-whi">Me gusta el pene</h1>
+  <Button class="bg-red-100 p-2" label="Submit" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from './App.vue' 
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+    },
+    unstyled: true,
+});
+
+app.mount('#app');
