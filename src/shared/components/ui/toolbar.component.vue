9f34a7d4940db84e8174fb4a5499924f6aa1cf82
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-12 16:41:13 -0500

    feat: add toolbar component with alerts and date display

---
+++ src/shared/components/ui/toolbar.component.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="h-20 bg-[#ebebeb] flex items-center justify-between px-4">
+    <h1 class="text-shadow-dark text-2xl tracking-wide">EcoGuardian Monitoring</h1>
+    <div class="flex flex-row items-center gap-2.5">
+      <div class="flex flex-row items-center h-9 rounded-md overflow-hidden">
+        <figure class="h-full flex items-center justify-center px-1.5 bg-slate-500">
+          <i class="pi pi-bell text-lg text-slate-50"></i>
+        </figure>
+        <button class="flex flex-row items-center px-3 gap-3 bg-slate-700 h-full">
+          <p class="text-gray-200 text-sm"><span class="text-white text-base font-normal mr-0.5">69</span> Alert</p>
+          <i class="pi pi-arrow-down text-xs rotate-235 text-slate-50"></i>
+        </button>
+      </div>
+      <div class="flex flex-row items-center gap-2.5 h-9 rounded-md overflow-hidden bg-gray-300/50 px-3">
+        <i class="pi pi-calendar text-lg text-gray-500 -mt-0.5"></i>
+        <span>Sunday, October 1, 2023</span>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
