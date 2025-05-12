88d76fb9ad4a2b3b52382305e31a9de9e4a00937
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-12 16:40:08 -0500

    style: format and organize imports in main.ts for improved readability

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue' 
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+
+// PrimeIcons
+import "primeicons/primeicons.css";
+
+// Routing
+import router from "./router/index.ts";
 
 const app = createApp(App);
-app.use(PrimeVue, {
+app
+  .use(PrimeVue, {
     theme: {
-        preset: Aura,
+      preset: Aura,
+      options: {
+        prefix: "p",
+        cssLayer: false,
+        darkModeSelector: "manual",
+      },
     },
     unstyled: true,
-});
+  })
+  .use(router);
 
-app.mount('#app');
+app.mount("#app");
