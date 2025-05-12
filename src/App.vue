a33818e3b5fa0c10486e930c8e5756c6a0b0ce9d
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-12 16:40:02 -0500

    refactor: remove unused Button component and update template structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
-<script setup lang="ts">
-import Button from "primevue/button"
-</script>
-
 <template>
-  <h1 class="text-whi">Me gusta el pene</h1>
-  <Button class="bg-red-100 p-2" label="Submit" />
+  <router-view></router-view>
 </template>
+
+<script setup lang="ts">
+</script>
 
 <style scoped></style>
