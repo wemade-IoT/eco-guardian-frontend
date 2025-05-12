76447c5b6e023707cdb0d25ca2ef44318d03cebd
Sebastian Ramirez Hoffmann <u202211894@upc.edu.pe>
2025-05-12 09:28:18 -0500

    feat: initial setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<h1 class="text-red-500 text-6xl">SEXO</h1>
+</template>
+
+<style scoped>
+
+</style>
