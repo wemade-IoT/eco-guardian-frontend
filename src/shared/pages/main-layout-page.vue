9343963bd32ac832779b8584e13f1674f5f0ccd1
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-12 16:41:22 -0500

    feat: add main layout page with sidebar and toolbar components

---
+++ src/shared/pages/main-layout-page.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="min-h-[100dvh] h-[100dvh] flex flex-row bg-[#ebebeb] overflow-hidden">
+    <sidebar></sidebar>
+    <div class="flex flex-col flex-1 overflow-hidden">
+      <toolbar />
+      <main class="flex-1 overflow-auto custom-scrollbar px-4">
+        <router-view></router-view>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Sidebar from '../components/ui/sidebar.component.vue';
+import Toolbar from '../components/ui/toolbar.component.vue';
+
+</script>
+
+<style scoped></style>
