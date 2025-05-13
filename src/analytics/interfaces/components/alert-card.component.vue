d09ee88a5407300214928dd8c2e76dcd1b4b36e5
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-13 01:46:44 -0500

    feat: update alert card component to format date and adjust alerts list to use created_at

--- src/analytics/interfaces/components/alert-card.component.vue
+++ src/analytics/interfaces/components/alert-card.component.vue
@@ -6,13 +6,21 @@ defineProps({
   content: { type: String, required: true },
   date: { type: String, required: true },
 });
+
+const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  if (isNaN(date.getTime())) {
+    return "Invalid Date";
+  }
+  return date.toLocaleString();
+};
 </script>
 
 <template>
-  <div class="alert-card bg-[#D6E899] border border-[#A4D39A] rounded p-4 flex flex-col">
+  <div class="alert-card bg-white rounded p-4 flex flex-col">
     <h3 class="text-[16px] font-semibold mb-2">{{ title }}</h3>
     <p class="text-[12px] font-normal mb-2">{{ content }}</p>
-    <p class="text-[12px] text-[#757575]">At {{ date }}</p>
+    <p class="text-[12px] text-[#757575]">At {{ formatDate(date) }}</p>
   </div>
 </template>
 
--- src/analytics/interfaces/components/alerts-list.component.vue
+++ src/analytics/interfaces/components/alerts-list.component.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="bg-gray-100 h-full p-4">
-    <h2 class="text-[18px] font-semibold mb-4">Notifications</h2>
+    <h2 class="text-xl font-semibold mb-4">Notifications</h2>
     <div class="flex flex-col gap-4">
       <alert-card
         v-for="(alert, index) in alerts"
         :key="index"
         :title="alert.title"
         :content="alert.content"
-        :date="alert.date"
+        :date="alert.created_at"
       />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { NotificationsService } from '../../../notifications/application/services/notifications.service';
 import AlertCard from './alert-card.component.vue';
 
-const alerts = [
-  { title: 'Watered Plantation', content: 'Watered plantation P-01.', date: '2023/10/01 12:25:20' },
-  { title: 'Humidity Below Threshold', content: 'Plantation P-01 needs watering.', date: '2023/10/02 12:20:00' },
-  { title: 'Temperature Low', content: 'Plantation P-03 temperature is below recommended value.', date: '12/05/2025 12:10:20' },
-];
+const alertsService = new NotificationsService();
+
+const alerts = ref<any[]>([]);
+
+onMounted(async () => {
+  try {
+    const response = await alertsService.getNotifications();
+    alerts.value = response;
+  } catch (error) {
+    console.error('Error fetching alerts:', error);
+  }
+});
 </script>
 
 <style scoped></style>
