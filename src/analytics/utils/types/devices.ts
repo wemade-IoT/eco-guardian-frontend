97cbc2bc650a8dd474ddce0af7d899c1db9881fa
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-12 19:31:39 -0500

    feat: add mock devices data and device state type definitions for analytics module

---
+++ src/analytics/utils/types/devices.ts
@@ -0,0 +1,8 @@
+export const DEVICE_STATE = {
+  ACTIVE: 'Active',
+  INACTIVE: 'Inactive',
+  LOST_SIGNAL: 'Lost Signal',
+  DECOMMISSIONED: 'DECOMMISSIONED',
+} as const;
+
+export type DEVICE_STATE = typeof DEVICE_STATE[keyof typeof DEVICE_STATE];
