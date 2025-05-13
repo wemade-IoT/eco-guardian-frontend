a896333e38cb27789e385f486b516ac627c76853
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-13 01:47:14 -0500

    feat: restructure analytics page to conditionally display devices or plants list based on user role

--- src/analytics/interfaces/pages/analytics-page.component.vue
+++ src/analytics/interfaces/pages/analytics-page.component.vue
@@ -4,7 +4,12 @@
       <lines-chart></lines-chart>
       <widgets-container></widgets-container>
     </div>
-    <devices-list></devices-list>
+    <div v-if="isEnterprise">
+      <devices-list></devices-list>
+    </div>
+    <div v-else>
+      <plants-list></plants-list>
+    </div>
     <div class="flex flex-col gap-2 h-full">
       <tips-carousel></tips-carousel>
       <alerts-list></alerts-list>
@@ -13,12 +18,18 @@
 </template>
 
 <script setup lang="ts">
+import { useAuthStore } from '../../../iam/interfaces/store/auth-store';
 import AlertsList from '../components/alerts-list.component.vue';
 import DevicesList from '../components/devices-list.component.vue';
 import LinesChart from '../components/lines-chart.component.vue';
+import PlantsList from '../components/plants-list.component.vue';
 import TipsCarousel from '../components/tips-carousel.component.vue';
 import WidgetsContainer from '../components/widgets-container.component.vue';
 
-</script>
+const authStore = useAuthStore();
 
+const isEnterprise = authStore.user?.role === 'ENTERPRISE';
+const isSpecialist = authStore.user?.role === 'SPECIALIST';
+const isDomestic = authStore.user?.role === 'DOMESTIC';
+</script>
 <style scoped></style>
