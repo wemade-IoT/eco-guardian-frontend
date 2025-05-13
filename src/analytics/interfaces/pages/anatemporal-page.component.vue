ca27d01461e1ba946e147da266445c52ea5f2e5e
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-12 21:34:38 -0500

    feat: add plant information component and update anatemporal page layout

--- src/analytics/interfaces/pages/anatemporal-page.component.vue
+++ src/analytics/interfaces/pages/anatemporal-page.component.vue
@@ -1,11 +1,11 @@
 <!-- este es para el dashboard de domestico mis apus -->
 <template>
-  <div class="grid grid-cols-[2fr_1fr_1fr] grid-rows-auto gap-2 h-max py-3">
+  <div class="grid grid-cols-[1fr_1fr_1fr] grid-rows-auto gap-2 h-max py-3">
     <div class="flex flex-col gap-2">
-      
-      <widgets-container></widgets-container>
+      <lines-chart></lines-chart>
+      <!-- <widgets-container></widgets-container> -->
     </div>
-    <devices-list></devices-list>
+    <plant-information></plant-information>
     <div class="flex flex-col gap-2 h-full">
       <tips-carousel></tips-carousel>
       <alerts-list></alerts-list>
@@ -17,6 +17,7 @@
 import AlertsList from '../components/alerts-list.component.vue';
 import DevicesList from '../components/devices-list.component.vue';
 import LinesChart from '../components/lines-chart.component.vue';
+import PlantInformation from '../components/plant-information.component.vue';
 import TipsCarousel from '../components/tips-carousel.component.vue';
 import WidgetsContainer from '../components/widgets-container.component.vue';
 </script>
