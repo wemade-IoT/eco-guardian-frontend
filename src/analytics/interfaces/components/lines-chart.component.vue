3792c10fd29bd0f5ec85a8d872ed74252cbc0497
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-12 19:31:15 -0500

    feat: implement lines chart component with responsive design and data selection

--- src/analytics/interfaces/components/lines-chart.component.vue
+++ src/analytics/interfaces/components/lines-chart.component.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="h-120 bg-gray-100">
-    <h2>Lines chart</h2>
-  </div>
+  <section class="bg-gray-100 p-4 w-full">
+    <div class="flex flex-col">
+      <div class="w-full flex flex-row items-center justify-between">
+        <h2 class="font-sans">Water and Energy Consumption over time</h2>
+        <Select v-model="selectedOption" :options="options" optionLabel="label" placeholder="Month"
+          class="flex flex-row items-center -space-x-2 tracking-wide rounded-md text-sm h-8" />
+      </div>
+      <div class="mt-2.5 relative before:absolute after:absolute before:bg-[#8fbf8f] 
+        after:bg-[#cde6cd] before:left-0 before:top-0 before:h-[1.5px] 
+        before:w-6 after:left-8 after:right-0 after:top-0 after:h-[1.5px]">
+      </div>
+      <div class="mt-6 w-full translate-x-1.5">
+        <Chart class="responsive-trick h-100" type="line" :data="chartData" :options="chartOptions" />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
+import { Select } from 'primevue';
+
+import { ref } from 'vue';
+
+import Chart from 'primevue/chart';
+import { chartData } from '../../utils/mocks/chart';
+import { chartOptions } from '../../utils/consts/chart';
+import type { PeriodsValue } from '../../utils/types/chart';
+
+const options = [
+  { label: 'Month', value: 'month' },
+  { label: 'Year', value: 'year' },
+];
+
+const selectedOption = ref<PeriodsValue>('month');
 
 </script>
 
-<style scoped></style>
+<style scoped>
+
+/* this is a trick to make the chart responsive omglul */
+.responsive-trick {
+  width: 99% !important;
+}
+</style>
