57e154629e7aa8b4bee1748499ed4f87118501f3
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-13 01:47:30 -0500

    feat: remove DECOMMISSIONED state from DEVICE_STATE constant

--- src/analytics/utils/types/devices.ts
+++ src/analytics/utils/types/devices.ts
@@ -2,7 +2,6 @@ export const DEVICE_STATE = {
   ACTIVE: 'Active',
   INACTIVE: 'Inactive',
   LOST_SIGNAL: 'Lost Signal',
-  DECOMMISSIONED: 'DECOMMISSIONED',
 } as const;
 
 export type DEVICE_STATE = typeof DEVICE_STATE[keyof typeof DEVICE_STATE];
