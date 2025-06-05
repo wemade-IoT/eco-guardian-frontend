ea22a8e6553584e89c4e6c9a1532745ad66de0c2
U202210138 <u202210138@upc.edu.pe>
2025-06-05 18:59:27 -0500

    feat: added profile page

---
+++ src/profile/interfaces/pages/profile-page.component.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import ProfileInfoCard from "@/profile/interfaces/components/profile-info-card.component.vue";
+import ProfileSubscriptionInfo from "@/profile/interfaces/components/profile-subscription-info.component.vue";
+import ProfilePayments from "@/profile/interfaces/components/profile-payments.component.vue";
+</script>
+
+<template>
+  <div class="flex flex-col ">
+    <profile-info-card></profile-info-card>
+    <profile-subscription-info></profile-subscription-info>
+    <profile-payments></profile-payments>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
