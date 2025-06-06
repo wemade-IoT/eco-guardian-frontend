89d3209cbc89d2f5f0f1ca0c3286c3c622adfb1d
U202210138 <u202210138@upc.edu.pe>
2025-06-05 19:18:21 -0500

    feat: added content for profile payments component

---
+++ src/profile/interfaces/components/profile-payments.component.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="mt-10 items-center justify-center flex flex-col gap-4">
+    <div class="widget-card bg-gray-100 border-[#fffff] rounded p-4 md:p-6 flex flex-col">
+      <div class="flex flex-col md:flex-row justify-between items-center">
+        <div class="flex flex-col w-full">
+          <div class="flex flex-col gap-2">
+            <p class="text-[21px] font-normal mb-1 text-black">Payments:</p>
+          </div>
+          <div class="flex flex-col sm:flex-row items-start sm:items-center gap-4 flex-wrap">
+            <div class="flex flex-col gap-4">
+              <p class="text-sm text-gray-500">Placeholder: Nicolas</p>
+              <p class="text-sm text-gray-500">Placeholder: Placeholder</p>
+            </div>
+            <div class="flex flex-col gap-4">
+              <button
+                  class="cursor-pointer bg-[#A4D39A] flex flex-row items-center gap-2 h-6 rounded-md overflow-hidden px-3">
+                <p class="text-sm text-gray-700 inline">Placeholder </p>
+              </button>
+              <button
+                  class="cursor-pointer bg-white flex flex-row items-center gap-2 h-5 w-36 rounded-md overflow-hidden px-4">
+                <p class="text-sm text-gray-500 text-center">Placeholder</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
