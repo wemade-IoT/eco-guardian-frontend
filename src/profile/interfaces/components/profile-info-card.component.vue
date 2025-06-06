37eb14d0813d8d3c86c51affec7cb7103281e153
U202210138 <u202210138@upc.edu.pe>
2025-06-05 19:15:33 -0500

    feat: added script for edit dialog on profile info card

---
+++ src/profile/interfaces/components/profile-info-card.component.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ProfileEditInfoDialog from './profile-edit-info-dialog.component.vue';
+
+
+interface UserData {
+  name: string;
+  lastName: string;
+  email: string;
+  photoUrl: string;
+}
+
+
+const userData = ref<UserData>({
+  name: 'Nicolas',
+  lastName: 'Esteban',
+  email: 'puerkito77@gmail.com',
+  photoUrl: 'https://avatars.githubusercontent.com/u/129230632?v=4'
+});
+
+
+const showEditDialog = ref(false);
+
+
+const openEditDialog = () => {
+  showEditDialog.value = true;
+};
+
+
+const handleSaveUserData = (updatedData: UserData) => {
+  userData.value = { ...updatedData };
+};
+</script>
