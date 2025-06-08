ed3b5731d1af0b90a33475315853c10c2f968588
Mathias <math.kuni123@gmail.com>
2025-06-07 19:10:55 -0500

    feat: add logo to left side login page

--- src/iam/interfaces/pages/login-page.component.vue
+++ src/iam/interfaces/pages/login-page.component.vue
@@ -1,40 +1,10 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <div class="w-full max-w-md p-8 bg-white rounded shadow-md">
-      <h1 class="mb-6 text-2xl font-bold text-center">Login</h1>
-      <form @submit.prevent="handleLogin">
-        <div class="mb-4">
-          <label for="email" class="block mb-2 text-sm font-medium text-gray-700">Email</label>
-          <input
-            type="email"
-            id="email"
-            v-model="email"
-            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Enter your email"
-          />
-        </div>
-        <div class="mb-6">
-          <label for="password" class="block mb-2 text-sm font-medium text-gray-700">Password</label>
-          <input
-            type="password"
-            id="password"
-            v-model="password"
-            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Enter your password"
-          />
-        </div>
-        <button
-          type="submit"
-          class="w-full px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-          Login
-        </button>
-      </form>
-      <div class="mt-4 text-center">
-        <p class="text-sm text-gray-600">
-          Don't have an account?
-          <router-link to="/register" class="text-blue-500 hover:underline">Create one</router-link>
-        </p>
+  <div class="flex flex-col md:flex-row min-h-screen">
+    <!-- LEFT-SIDE  -->
+    <div class="w-full h-screen md:h-auto md:w-1/2 bg-[#FAF5E7] flex flex-col justify-start p-8 md:p-16 relative">
+      <!-- Logo -->
+      <div class="absolute top-10 left-10">
+        <img src="/src/assets/images/logo-ecoguardian.png" alt="EcoGuardian Logo" class="h-16" />
       </div>
     </div>
   </div>
@@ -43,20 +13,30 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useAuthStore } from "../store/auth-store";
+import { useRouter } from "vue-router";
 
 const email = ref("");
 const password = ref("");
+const router = useRouter();
 
 const handleLogin = async () => {
   const authStore = useAuthStore();
 
   try {
     await authStore.login(email.value, password.value);
-    console.log("Login successful");
+    router.push("/home");
   } catch (error) {
     console.error("Login failed:", error);
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@media (max-width: 768px) {
+
+  input,
+  button {
+    font-size: 16px;
+  }
+}
+</style>
