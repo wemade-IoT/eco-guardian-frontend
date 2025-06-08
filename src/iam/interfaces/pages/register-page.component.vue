05858e88f440a33836d11b322fdbe660831e742a
Mathias <math.kuni123@gmail.com>
2025-06-07 19:07:59 -0500

    feat: add registration form with necessary fields

--- src/iam/interfaces/pages/register-page.component.vue
+++ src/iam/interfaces/pages/register-page.component.vue
@@ -6,6 +6,55 @@
       <div class="absolute top-10 left-10">
         <img src="/src/assets/images/logo-ecoguardian.png" alt="EcoGuardian Logo" class="h-16" />
       </div>
+
+      <!-- FORMULARIO -->
+      <div class="max-w-md mt-36">
+        <h1 class="text-3xl font-bold mb-2">Welcome to<br>EcoGuardian Platform</h1>
+        <p class="mb-8 md:mt-0 mt-10">Enter your information</p>
+
+        <form @submit.prevent="handleRegister" class="space-y-9">
+          <!-- NAME -->
+          <div class="flex gap-4">
+            <div class="w-1/2">
+              <label for="name" class="block text-sm font-medium mb-2">Name *</label>
+              <input type="text" id="name" v-model="name" class="w-full p-3 rounded bg-white placeholder:text-gray-400"
+                placeholder="name" required />
+            </div>
+            <div class="w-1/2">
+              <label for="lastName" class="block text-sm font-medium mb-2">Last Name *</label>
+              <input type="text" id="lastName" v-model="lastName" class="w-full p-3 rounded bg-white placeholder:text-gray-400"
+                placeholder="lastname" required />
+            </div>
+          </div>
+          
+          <!-- EMAIL -->
+          <div>
+            <label for="email" class="block text-sm font-medium mb-2">Email *</label>
+            <input type="email" id="email" v-model="email" class="w-full p-3 rounded bg-white placeholder:text-gray-400"
+              placeholder="email" required />
+          </div>
+          
+          <!-- PASSWORD -->
+          <div class="flex gap-4">
+            <div class="w-1/2">
+              <label for="password" class="block text-sm font-medium mb-2">Password *</label>
+              <input type="password" id="password" v-model="password"
+                class="w-full p-3 rounded bg-white placeholder:text-gray-400" placeholder="password" required />
+            </div>
+            <div class="w-1/2">
+              <label for="confirmPassword" class="block text-sm font-medium mb-2">Confirm password *</label>
+              <input type="password" id="confirmPassword" v-model="confirmPassword"
+                class="w-full p-3 rounded bg-white placeholder:text-gray-400" placeholder="confirm password" required />
+            </div>
+          </div>
+          
+          <div class="pt-4">
+            <button type="submit" class="w-full py-3 bg-[#578257] text-white rounded hover:bg-[#4a6b4a] md:mt-0 mt-16">
+              Enter
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
